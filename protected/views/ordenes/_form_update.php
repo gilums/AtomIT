90efef12cd91a2fcf19c4aa5578c8ac1d84bfa8f
Gaston Baldenegro <gbg933@gmail.com>
2015-10-20 23:42:42 -0300

    otro

--- protected/views/ordenes/_form_update.php
+++ protected/views/ordenes/_form_update.php
@@ -110,7 +110,7 @@ if ($model->isNewRecord==false) {
                 <div class="form-group">
                     <div class="col-lg-12">
                         <?php echo CHtml::submitButton('Guardar',array('class'=>'btn btn-primary')); ?>
-                        <?php echo CHtml::submitButton('Guardar/Imprimir',array('class'=>'btn btn-primary','name'=>'upandimp')); ?>
+                        <?php echo CHtml::submitButton('Guardar/Imprimir',array('class'=>'btn btn-primary','name'=>'upandimp','target'=>'_blank')); ?>
                     </div>
                 </div>
             </div>
